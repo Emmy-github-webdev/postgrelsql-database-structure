26ea4dd26039444c6da824d10fd927adb35f3801
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-17 20:18:57 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INTEGER,
+	neutered BOOLEAN NOT NULL,
+	weight_kg DECIMAL	
+)
