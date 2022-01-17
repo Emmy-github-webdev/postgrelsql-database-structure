a6ec560efd364dd22a9f79f1f3a34bbab2282d00
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-17 21:20:45 +0100

    Update neutered data

--- data.sql
+++ data.sql
@@ -9,6 +9,6 @@ INSERT INTO animals(
 )
 
 VALUES('Agumon', DATE '2020-02-03', '0', 'true', '10.23');
-VALUES('Gabumon', DATE '2018-11-15', '1', 'true', '8.00');
+VALUES('Gabumon', DATE '2018-11-15', '2', 'true', '8.00');
 VALUES('Pikachu', DATE '2021-01-07', '1', 'false', '15.00');
-VALUES('Devimon', DATE '2017-05-12', '1', 'true', '11.00');
+VALUES('Devimon', DATE '2017-05-12', '5', 'true', '11.00');
