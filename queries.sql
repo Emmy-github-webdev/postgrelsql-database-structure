8bc0fd29726bba483dfc1f5594fb2ec5da7523a4
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-18 18:29:21 +0100

    Update species columns to pokemon for animals that ends with mon

--- queries.sql
+++ queries.sql
@@ -8,3 +8,10 @@ SELECT name, escape_attempts  FROM animals WHERE weight_kg > '10.5';
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+/*Queries, update animals table.*/
+
+UPDATE animals 
+SET species = 'digimon'
+WHERE name LIKE '%mon';
